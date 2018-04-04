e2cedf24628c7531065013e1f9e85574e82dc4e2
ian0549 <iannakoto@yahoo.com>
2018-04-03 18:14:19 -0700

    Update config.py

--- GhClimHub/config.py
+++ GhClimHub/config.py
@@ -2,7 +2,7 @@ import ee
 
 import os
 
-service_account = 'ghclimhub@appspot.gserviceaccount.com'
+
 
 credentials = ee.ServiceAccountCredentials(service_account, os.path.join(os.path.dirname(os.path.realpath(__file__)),'privatekey.json'))
 
